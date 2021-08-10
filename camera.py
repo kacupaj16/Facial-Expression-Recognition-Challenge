b19debe37e90f51a95194148a64cd2c4907cffcb
Kriselda Kaçupaj <kriselda.kacupaj@gmail.com>
2021-08-10 17:17:25 +0300

    Multithreading

--- camera.py
+++ camera.py
@@ -1,46 +1,72 @@
 import cv2
 from model import FacialExpressionModel
 import numpy as np
+from threading import Thread
+from datetime import datetime
 
-rgb = cv2.VideoCapture(0)
-facec = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-font = cv2.FONT_HERSHEY_SIMPLEX
+# rgb = cv2.VideoCapture(0)
+# facec = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# font = cv2.FONT_HERSHEY_SIMPLEX
 
-def __get_data__():
-    """
-    __get_data__: Gets data from the VideoCapture object and classifies them
-    to a face or no face. 
+class DataRetriever:
+    def __init__(self,src=0):
+        self.startTime = datetime.now()
+        self.runningTime = 30               #seconds
+        self.rgb =  cv2.VideoCapture(src)
+        self.facec = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        self.font = cv2.FONT_HERSHEY_SIMPLEX
+        (_, self.frame) = self.rgb.read()
+        self.gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+        self.faces = self.facec.detectMultiScale(self.gray, 1.3, 5)
+
+    def start(self):
+        Thread(target=self.__get_data__, args=()).start()
+        return self
+
+    def __get_data__(self):
+        (_, self.frame) = self.rgb.read()
+        self.gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+        self.faces = self.facec.detectMultiScale(self.gray, 1.3, 5)
+
+class Displayer:
+    def __init__(self,frame=np.zeros((48,48))):
+        self.frame = frame
+        self.done = False
     
-    returns: tuple (faces in image, frame read, grayscale frame)
-    """
-    _, fr = rgb.read()
-    gray = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
-    faces = facec.detectMultiScale(gray, 1.3, 5)
+    def start(self):
+        Thread(target=self.display,args=()).start()
+        return self
+    def display(self):
+        while not self.done:
+            cv2.imshow('Filter', self.frame)
+            if cv2.waitKey(1) == 27:
+                self.done = True
     
-    return faces, fr, gray
+    def stop(self):
+        self.done = True
+        cv2.destroyAllWindows()
 
-def start_app(cnn):
-    skip_frame = 10
-    data = []
-    flag = False
-    ix = 0
-    while True:
-        ix += 1
-        
-        faces, fr, gray_fr = __get_data__()
+
+def start_app(cnn,src=0):
+    data_retriver = DataRetriever(src).start()
+    displayer = Displayer().start()
+    while (datetime.now()-data_retriver.startTime).seconds<data_retriver.runningTime:
+        data_retriver.__get_data__()
+        frame = data_retriver.frame
+        gray = data_retriver.gray
+        faces = data_retriver.faces
+
         for (x, y, w, h) in faces:
-            fc = gray_fr[y:y+h, x:x+w]
+            fc = gray[y:y+h, x:x+w]
             
             roi = cv2.resize(fc, (48, 48))
             pred = cnn.predict_emotion(roi[np.newaxis, :, :, np.newaxis])
 
-            cv2.putText(fr, pred, (x, y), font, 1, (255, 255, 0), 2)
-            cv2.rectangle(fr,(x,y),(x+w,y+h),(255,0,0),2)
+            cv2.putText(frame, pred, (x, y), data_retriver.font, 1, (255, 255, 0), 2)
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
 
-        if cv2.waitKey(1) == 27:
-            break
-        cv2.imshow('Filter', fr)
-    cv2.destroyAllWindows()
+        displayer.frame=frame
+    displayer.stop()
 
 
 if __name__ == '__main__':
