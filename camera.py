0cceb160fa1da4e4351c7602c5cef2a278446d52
Kriselda Kaçupaj <kriselda.kacupaj@gmail.com>
2021-08-11 13:48:02 +0300

    Threads added

--- camera.py
+++ camera.py
@@ -2,22 +2,16 @@ import cv2
 from model import FacialExpressionModel
 import numpy as np
 from threading import Thread
+import concurrent
 from datetime import datetime
+from time import sleep
 
-# rgb = cv2.VideoCapture(0)
-# facec = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-# font = cv2.FONT_HERSHEY_SIMPLEX
 
 class DataRetriever:
     def __init__(self,src=0):
-        self.startTime = datetime.now()
-        self.runningTime = 30               #seconds
         self.rgb =  cv2.VideoCapture(src)
-        self.facec = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        self.font = cv2.FONT_HERSHEY_SIMPLEX
-        (_, self.frame) = self.rgb.read()
-        self.gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
-        self.faces = self.facec.detectMultiScale(self.gray, 1.3, 5)
+        self.fps = self.rgb.get(cv2.cv2.CAP_PROP_FPS)
+        self.count = 0
 
     def start(self):
         Thread(target=self.__get_data__, args=()).start()
@@ -25,13 +19,16 @@ class DataRetriever:
 
     def __get_data__(self):
         (_, self.frame) = self.rgb.read()
-        self.gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
-        self.faces = self.facec.detectMultiScale(self.gray, 1.3, 5)
+        self.count+=1
+    
+    def close(self):
+        self.rgb.release()
 
 class Displayer:
-    def __init__(self,frame=np.zeros((48,48))):
+    def __init__(self,frame=np.zeros((480,640))):
         self.frame = frame
         self.done = False
+        self.count = 0
     
     def start(self):
         Thread(target=self.display,args=()).start()
@@ -47,27 +44,70 @@ class Displayer:
         cv2.destroyAllWindows()
 
 
+class Predicter:
+    def __init__(self,cnn,displayer):
+        self.frame = None
+        self.faces = None
+        self.gray = None
+        self.newFrame = False
+        self.done = False
+        self.count = 0
+        self.font = cv2.FONT_HERSHEY_SIMPLEX
+        self.cnn = cnn
+        self.displayer = displayer
+        self.facec = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        self.gray = None
+        self.faces = None
+
+    
+    def start(self,):
+        Thread(target=self.predict,args=()).start()
+        return self
+
+    def predict(self):
+        while not self.done:
+            if self.newFrame:
+                self.gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+                self.faces = self.facec.detectMultiScale(self.gray, 1.3, 5)
+                for (x, y, w, h) in self.faces:
+                        fc = self.gray[y:y+h, x:x+w]
+                        
+                        roi = cv2.resize(fc, (48, 48))
+                        pred = self.cnn.predict_emotion(roi[np.newaxis, :, :, np.newaxis])
+
+                        cv2.putText(self.frame, pred, (x, y), self.font, 1, (255, 255, 0), 2)
+                        cv2.rectangle(self.frame,(x,y),(x+w,y+h),(255,0,0),2)
+                self.displayer.frame = self.frame
+                cv2.imwrite('f'+str(self.count).zfill(4) + '.jpg',self.frame)
+                self.count +=1
+                self.newFrame = False
+
+    def set_frame(self,frame):
+        self.frame = frame
+        self.newFrame = True        
+    
+    def stop(self,):
+        self.done = True
+        print(self.count)
+
+
 def start_app(cnn,src=0):
+    startTime = datetime.now()
+    runningTime = 60
     data_retriver = DataRetriever(src).start()
+    
+    print("Frames per second using video.get(cv2.cv.CV_CAP_PROP_FPS): {0}".format(data_retriver.fps))
     displayer = Displayer().start()
-    while (datetime.now()-data_retriver.startTime).seconds<data_retriver.runningTime:
+    predicter = Predicter(cnn,displayer).start()
+
+
+    while (datetime.now()-startTime).seconds<runningTime:
         data_retriver.__get_data__()
-        frame = data_retriver.frame
-        gray = data_retriver.gray
-        faces = data_retriver.faces
-
-        for (x, y, w, h) in faces:
-            fc = gray[y:y+h, x:x+w]
-            
-            roi = cv2.resize(fc, (48, 48))
-            pred = cnn.predict_emotion(roi[np.newaxis, :, :, np.newaxis])
-
-            cv2.putText(frame, pred, (x, y), data_retriver.font, 1, (255, 255, 0), 2)
-            cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-
-        displayer.frame=frame
+        predicter.set_frame(data_retriver.frame)
+    data_retriver.close()
     displayer.stop()
-
+    sleep(20)
+    predicter.stop()
 
 if __name__ == '__main__':
     model = FacialExpressionModel("face_model.json", "face_model.h5")
